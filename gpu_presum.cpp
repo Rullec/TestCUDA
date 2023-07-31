0f554c29876674c6f56259bf6fe16b05c90378a9
xudong <xudongfeng18@gmail.com>
2023-07-31 11:48:36 +0800

    avaliable for float types

--- gpu_presum.cpp
+++ gpu_presum.cpp
@@ -1,23 +1,28 @@
 #include "gpu_utils/CudaArray.h"
 #include "gpu_utils/CudaUtil.h"
 #include "utils/EigenUtil.h"
+#include <type_traits>
 
-extern void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
-                          cCudaArray<int> &x_gpu,
-                          cCudaArray<int> &x_presum_gpu, bool is_inclusive_prefix_sum);
-int main()
+template <typename dtype>
+void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
+                   cCudaArray<dtype> &x_gpu, cCudaArray<dtype> &x_presum_gpu,
+                   bool is_inclusive_prefix_sum);
+template <typename dtype> void Test(int N)
 {
     int sm_bytes = cCudaUtil::GetSharedMemoryBytes();
     int max_thread = 32;
     bool is_inclusive_prefix_sum = false;
-    int N = 1000;
-    tVectorXi x = tVectorXi::Random(N);
-    for (int i = 0; i < N; i++)
+    using vec = Eigen::Matrix<dtype, -1, 1>;
+    vec x = vec::Random(N);
+    if constexpr (std::is_same_v<dtype, int> ||
+                  std::is_same_v<dtype, unsigned int>)
     {
-        x[i] = x[i] % 10;
-        // x[i] = 1;
+
+        for (int i = 0; i < N; i++)
+            x[i] = x[i] % 10;
     }
-    tVectorXi x_presum_cpu = tVectorXi::Zero(N);
+
+    vec x_presum_cpu = vec::Zero(N);
 
     if (is_inclusive_prefix_sum == false)
     {
@@ -33,27 +38,58 @@ int main()
     std::cout << "x = " << x.transpose() << std::endl;
     std::cout << "x cpu presum = " << x_presum_cpu.transpose() << std::endl;
     // 1. upload to GPU
-    cCudaArray<int> x_gpu;
-    cCudaArray<int> x_presum_gpu;
+    cCudaArray<dtype> x_gpu;
+    cCudaArray<dtype> x_presum_gpu;
     x_gpu.Resize(N);
     x_gpu.Upload(x.data(), x.size());
     x_presum_gpu.Resize(N);
     // 2.
-    CalcPresumGPU(sm_bytes, max_thread, x_gpu, x_presum_gpu, is_inclusive_prefix_sum);
-    std::vector<int> x_presum_gpu_downloaded;
+    CalcPresumGPU<dtype>(sm_bytes, max_thread, x_gpu, x_presum_gpu,
+                         is_inclusive_prefix_sum);
+    std::vector<dtype> x_presum_gpu_downloaded;
     x_presum_gpu.Download(x_presum_gpu_downloaded);
     printf("x gpu presum =");
     for (auto &x : x_presum_gpu_downloaded)
-        printf("%d ", x);
+    {
+
+        if constexpr (std::is_same_v<dtype, int> ||
+                      std::is_same_v<dtype, unsigned int>)
+            printf("%d ", x);
+        else
+            printf("%.5f ", x);
+    }
     printf("\n");
 
+    bool test_failed = false;
     for (int i = 0; i < N; i++)
     {
-        if (std::fabs(x_presum_cpu[i] - x_presum_gpu_downloaded[i]) > 1e-5)
+        if (std::fabs(x_presum_cpu[i] - x_presum_gpu_downloaded[i]) > 1e-3)
         {
-            printf("error: result cpu %d %d != %d\n", i, x_presum_cpu[i],
-                   x_presum_gpu_downloaded[i]);
+            test_failed = true;
+
+            if constexpr (std::is_same_v<dtype, int> ||
+                          std::is_same_v<dtype, unsigned int>)
+            {
+                printf("error: result cpu %d %d != %d\n", i, x_presum_cpu[i],
+                       x_presum_gpu_downloaded[i]);
+            }
+            else
+            {
+                printf("error: result cpu %d %.5f != %.5f\n", i,
+                       x_presum_cpu[i], x_presum_gpu_downloaded[i]);
+            }
         }
     }
-    printf("succ\n");
+    if (test_failed == false)
+        printf("succ\n");
+    else
+    {
+        printf("failed!\n");
+        exit(1);
+    }
+}
+int main()
+{
+    // Test<int>(100);
+    Test<float>(1000);
 }
--- gpu_presum.cu
+++ gpu_presum.cu
@@ -2,18 +2,22 @@
 #include "gpu_utils/CudaDevPtr.h"
 #include <cfloat>
 #include <climits>
-__global__ void ApplyOffsetToBlocks(int num_of_data, devPtr<int> raw_data,
-                                    devPtr<const int> per_block_offset_arr)
+
+template <typename dtype>
+__global__ void ApplyOffsetToBlocks(int num_of_data, devPtr<dtype> raw_data,
+                                    devPtr<const dtype> per_block_offset_arr)
 {
     CUDA_function;
     int tid_global = threadIdx.x + blockDim.x * blockIdx.x;
-    int tid_local = threadIdx.x;
+    // int tid_local = threadIdx.x;
     if (tid_global >= num_of_data)
         return;
     raw_data[tid_global] += per_block_offset_arr[blockIdx.x];
 }
-__global__ void AddTwoArray(int num_of_data, devPtr<int> data0,
-                            devPtr<const int> data1)
+
+template <typename dtype>
+__global__ void AddTwoArray(int num_of_data, devPtr<dtype> data0,
+                            devPtr<const dtype> data1)
 {
     CUDA_function;
     int tid_global = threadIdx.x + blockDim.x * blockIdx.x;
@@ -22,9 +26,10 @@ __global__ void AddTwoArray(int num_of_data, devPtr<int> data0,
     data0[tid_global] += data1[tid_global];
 }
 
+template <typename dtype>
 __global__ void CalcPresumGPUKernel_Blelloch(
-    int num_of_data, devPtr<int> raw_data, devPtr<int> tar_data,
-    devPtr<int> block_offset_final, bool output_to_block_offset_final,
+    int num_of_data, devPtr<dtype> raw_data, devPtr<dtype> tar_data,
+    devPtr<dtype> block_offset_final, bool output_to_block_offset_final,
     bool inplace_calculation, bool inclusive_prefix_sum)
 {
     CUDA_function;
@@ -33,7 +38,11 @@ __global__ void CalcPresumGPUKernel_Blelloch(
     int tid_local = threadIdx.x;
     // if(tid_global == 0)
     //     printf("step1\n");
-    extern __shared__ int smem[];
+    // extern __shared__ dtype smem[];
+
+    extern __shared__ __align__(sizeof(dtype)) unsigned char my_smem[];
+    dtype *smem = reinterpret_cast<dtype *>(my_smem);
+
     smem[tid_local] = 0;
 
     int num_of_data_this_block = 0;
@@ -89,7 +98,7 @@ __global__ void CalcPresumGPUKernel_Blelloch(
     //     printf("step1.4\n");
 
     // 2. inverse pass
-    int last_sum = smem[tid_local];
+    dtype last_sum = smem[tid_local];
     // 2.1 set root to zero
     if (tid_local == (two_power_capacity - 1))
         smem[tid_local] = 0;
@@ -100,14 +109,14 @@ __global__ void CalcPresumGPUKernel_Blelloch(
     // now, stepsize == num_of_data
     while (stepsize >= 2)
     {
-        unsigned int gap = stepsize;
+        // unsigned int gap = stepsize;
         // if (tid == 0)
         //     printf("---inverse stepsize %d---\n", stepsize);
         if ((tid_local + 1) % stepsize == 0)
         {
             unsigned int child_id_left = tid_local - stepsize / 2;
 
-            int left_val = smem[child_id_left];
+            dtype left_val = smem[child_id_left];
             // printf("raw left[%d] = %d, right[%d] = %d\n", child_id_left,
             // smem[child_id_left], tid_local, smem[tid_local]);
 
@@ -135,16 +144,23 @@ __global__ void CalcPresumGPUKernel_Blelloch(
     // num_of_data);
     if (tid_global < num_of_data)
     {
-        int cur_val = (inclusive_prefix_sum == false)
-                          ? smem[tid_local]
-                          : (((tid_local + 1) != num_of_data_this_block)
-                                 ? (smem[tid_local + 1])
-                                 : last_sum);
+        dtype cur_val = (inclusive_prefix_sum == false)
+                            ? smem[tid_local]
+                            : (((tid_local + 1) != num_of_data_this_block)
+                                   ? (smem[tid_local + 1])
+                                   : last_sum);
 
         if (inplace_calculation == false)
         {
             tar_data[tid_global] = cur_val;
-            // printf("write to tar %d = %d\n", tid_global, smem[tid_local]);
+
+            // if constexpr (std::is_same_v<dtype, int> ||
+            //               std::is_same_v<dtype, unsigned int>)
+            //     printf("write to tar %d = %d\n", tid_global,
+            //     smem[tid_local]);
+            // else
+            //     printf("write to tar %d = %.3f\n", tid_global,
+            //     smem[tid_local]);
         }
         else
         {
@@ -178,18 +194,18 @@ int calc_num_thread_for_scan_two_power(int num_of_data, int max_sm_size,
     return num_thread;
 }
 
+template <typename dtype>
 void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
-                   cCudaArray<int> &x_gpu, cCudaArray<int> &x_presum_gpu,
+                   cCudaArray<dtype> &x_gpu, cCudaArray<dtype> &x_presum_gpu,
                    bool is_inclusive_prefix_sum)
 {
-    CUDA_function;
     int num_of_data = x_gpu.Size();
 
     int num_thread = calc_num_thread_for_scan_two_power(
         num_of_data, shared_mem_size_bytes, max_thread);
 
-    int sm_bytes = num_thread * sizeof(int);
-    printf("num thread %d\n", num_thread);
+    int sm_bytes = num_thread * sizeof(dtype);
+    // printf("num thread %d\n", num_thread);
     if (num_thread < num_of_data)
     {
         // printf("error: current thread %d cannot handle data %d\n",
@@ -197,7 +213,7 @@ void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
         //        num_of_data);
         // exit(1);
 
-        cCudaArray<int> block_offset_buf;
+        cCudaArray<dtype> block_offset_buf;
         int num_blocks =
             (num_of_data / num_thread) + ((num_of_data % num_thread) != 0);
         block_offset_buf.Resize(num_blocks);
@@ -211,7 +227,7 @@ void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
 
         int num_of_thread_for_blocks = calc_num_thread_for_scan_two_power(
             num_blocks, shared_mem_size_bytes, max_thread);
-        int sm_bytes_for_blocks = num_of_thread_for_blocks * sizeof(int);
+        int sm_bytes_for_blocks = num_of_thread_for_blocks * sizeof(dtype);
 
         if (num_blocks > num_thread)
         {
@@ -222,27 +238,34 @@ void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
             exit(1);
         }
 
-        cCudaArray<int> del;
+        cCudaArray<dtype> del;
         CalcPresumGPUKernel_Blelloch CUDA_at_SM(
             num_blocks, num_of_thread_for_blocks, sm_bytes_for_blocks)(
             num_blocks, block_offset_buf.Ptr(), block_offset_buf.Ptr(),
             del.Ptr(), false, true, false);
 
-        std::vector<int> block_offset_buf_cpu;
+        std::vector<dtype> block_offset_buf_cpu;
         block_offset_buf.Download(block_offset_buf_cpu);
         printf("block_offset_buf = ");
         for (auto &x : block_offset_buf_cpu)
-            printf("%d ", x);
+        {
+
+            if constexpr (std::is_same_v<dtype, int> ||
+                          std::is_same_v<dtype, unsigned int>)
+                printf("%d ", x);
+            else
+                printf("%.3f ", x);
+        }
         printf("\n");
         CUDA_ERR("CalcPresumGPUKernel_Blelloch");
-        ApplyOffsetToBlocks CUDA_at(num_of_data, num_thread)(
+        ApplyOffsetToBlocks<dtype> CUDA_at(num_of_data, num_thread)(
             num_of_data, x_presum_gpu.Ptr(), block_offset_buf.Ptr());
         CUDA_ERR("ApplyOffsetToBlocks");
     }
     else
     {
         // 1. determine the num of thread = sm_bytes
-        cCudaArray<int> buf;
+        cCudaArray<dtype> buf;
         CalcPresumGPUKernel_Blelloch CUDA_at_SM(num_of_data, num_thread,
                                                 sm_bytes)(
             num_of_data, x_gpu.Ptr(), x_presum_gpu.Ptr(), buf.Ptr(), false,
@@ -250,3 +273,12 @@ void CalcPresumGPU(int shared_mem_size_bytes, int max_thread,
         CUDA_ERR("CalcPresumGPU");
     }
 }
+
+template void CalcPresumGPU<int>(int shared_mem_size_bytes, int max_thread,
+                                 cCudaArray<int> &x_gpu,
+                                 cCudaArray<int> &x_presum_gpu,
+                                 bool is_inclusive_prefix_sum);
+template void CalcPresumGPU<float>(int shared_mem_size_bytes, int max_thread,
+                                   cCudaArray<float> &x_gpu,
+                                   cCudaArray<float> &x_presum_gpu,
+                                   bool is_inclusive_prefix_sum);
